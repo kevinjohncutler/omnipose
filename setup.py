b7ed91e5bc6c82248f281d0ae57aebd9f3b8e904
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2022-01-05 18:17:06 -0800

    Omnipose now standalone package to be installed on top of Cellpose.

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from setuptools import setup
+
+install_deps = ['numpy>=1.20.0', 'scipy', 'numba', 
+                'edt','scikit-image','cellpose']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="omnipose",
+    author="Kevin Cutler",
+    author_email="kevinjohncutler@outlook.com",
+    description="cell segmentation algorithm improving on the Cellpose framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevinjohncutler/ncolor",
+    setup_requires=[
+      'pytest-runner',
+      'setuptools_scm',
+    ],
+    packages=setuptools.find_packages(),
+    use_scm_version=True,
+    install_requires = install_deps,
+    tests_require=[
+      'pytest'
+    ],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    )
+)
