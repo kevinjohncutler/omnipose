f27b117ba5ae669819b28b4106d34e215592fd8c
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2022-01-24 14:58:14 -0800

    Code refactoring from Cellpose. Initial generalization of distance/flow to ND (GPU only for now).

--- omnipose/.ipynb_checkpoints/utils-checkpoint.py
+++ omnipose/.ipynb_checkpoints/utils-checkpoint.py
@@ -4,6 +4,8 @@ from scipy.ndimage.morphology import binary_dilation, binary_erosion
 from scipy.ndimage import label # used as alternative to skimage.measure.label, need to test speed and output...
 import fastremap
 
+
+
 try:
     from skimage import measure
     SKIMAGE_ENABLED = True 
@@ -62,7 +64,7 @@ def clean_boundary(labels,boundary_thickness=3,area_thresh=30):
 # Should work for 3D too. Could put into usigned integer form at the end... 
 # Also could use some parallelization 
 
-def format_labels(labels, clean=False, min_area=9):
+def format_labels(labels, clean=False, min_area=9, verbose=True):
     """
     Puts labels into 'standard form', i.e. background=0 and cells 1,2,3,...,N-1,N.
     Optional clean flag: disconnect and disjoint masks and discard small masks beflow min_area. 
@@ -87,21 +89,25 @@ def format_labels(labels, clean=False, min_area=9):
                 regions = measure.regionprops(lbl)
                 regions.sort(key=lambda x: x.area, reverse=True)
                 if len(regions) > 1:
-                    print('Warning - found mask with disjoint label.')
+                    if verbose:
+                        print('Warning - found mask with disjoint label.')
                     for rg in regions[1:]:
                         if rg.area <= min_area:
                             labels[rg.coords[:,0], rg.coords[:,1]] = 0
-                            print('secondary disjoint part smaller than min_area. Removing it.')
+                            if verbose:
+                                print('secondary disjoint part smaller than min_area. Removing it.')
                         else:
-                            print('secondary disjoint part bigger than min_area, relabeling. Area:',rg.area, 
-                                    'Label value:',np.unique(labels[rg.coords[:,0], rg.coords[:,1]]))
-                            labels[rg.coords[:,0], rg.coords[:,1]] = np.max(labels)+1
+                            if verbose:
+                                print('secondary disjoint part bigger than min_area, relabeling. Area:',rg.area, 
+                                        'Label value:',np.unique(labels[tuple(rg.coords.T)]))
+                            labels[tuple(rg.coords.T)] = np.max(labels)+1
                             
                 rg0 = regions[0]
                 if rg0.area <= min_area:
-                    labels[rg0.coords[:,0], rg0.coords[:,1]] = 0
-                    print('Warning - found mask area less than', min_area)
-                    print('Removing it.')
+                    labels[tuple(rg0.coords.T)] = 0
+                    if verbose:
+                        print('Warning - found mask area less than', min_area)
+                        print('Removing it.')
             else:
                 connectivity_shape = np.array([3 for i in range(mask.ndim)])
                 lbl = label(mask, connectivity=np.ones(connectivity_shape))[0]
@@ -111,5 +117,97 @@ def format_labels(labels, clean=False, min_area=9):
     labels = fastremap.refit(labels) # put into smaller data type if possible 
     return labels
 
+# get the number of m-dimensional hypercubes connected to the n-cube
+def cubestats(n):
+    faces = []
+    for m in range(n+1):
+          faces.append((2**(n-m))*math.comb(n,m))
+    return faces
 
 
+# duplicated from cellpose temporarily 
+def fill_holes_and_remove_small_masks(masks, min_size=15, hole_size=3, scale_factor=1):
+    """ fill holes in masks (2D/3D) and discard masks smaller than min_size (2D)
+    
+    fill holes in each mask using scipy.ndimage.morphology.binary_fill_holes
+    
+    Parameters
+    ----------------
+
+    masks: int, 2D or 3D array
+        labelled masks, 0=NO masks; 1,2,...=mask labels,
+        size [Ly x Lx] or [Lz x Ly x Lx]
+
+    min_size: int (optional, default 15)
+        minimum number of pixels per mask, can turn off with -1
+
+    Returns
+    ---------------
+
+    masks: int, 2D or 3D array
+        masks with holes filled and masks smaller than min_size removed, 
+        0=NO masks; 1,2,...=mask labels,
+        size [Ly x Lx] or [Lz x Ly x Lx]
+    
+    """
+
+    if masks.ndim==2:
+        # formatting to integer is critical
+        # need to test how it does with 3D
+        masks = ncolor.format_labels(masks, min_area=min_size)
+        
+    hole_size *= scale_factor
+        
+    if masks.ndim > 3 or masks.ndim < 2:
+        raise ValueError('masks_to_outlines takes 2D or 3D array, not %dD array'%masks.ndim)
+    
+    slices = find_objects(masks)
+    j = 0
+    for i,slc in enumerate(slices):
+        if slc is not None:
+            msk = masks[slc] == (i+1)
+            npix = msk.sum()
+            if min_size > 0 and npix < min_size:
+                masks[slc][msk] = 0
+            else:   
+                hsz = np.count_nonzero(msk)*hole_size/100 #turn hole size into percentage
+                #eventually the boundary output should be used to properly exclude real holes vs label gaps 
+                if msk.ndim==3:
+                    for k in range(msk.shape[0]):
+                        # Omnipose version (breaks 3D tests)
+                        # padmsk = remove_small_holes(np.pad(msk[k],1,mode='constant'),hsz)
+                        # msk[k] = padmsk[1:-1,1:-1]
+                        
+                        #Cellpose version
+                        msk[k] = binary_fill_holes(msk[k])
+
+                else:          
+                    if SKIMAGE_ENABLED: # Omnipose version (passes 2D tests)
+                        padmsk = remove_small_holes(np.pad(msk,1,mode='constant'),hsz)
+                        msk = padmsk[1:-1,1:-1]
+                    else: #Cellpose version
+                        msk = binary_fill_holes(msk)
+                masks[slc][msk] = (j+1)
+                j+=1
+    return masks
+
+
+    # if masks.ndim > 3 or masks.ndim < 2:
+    #     raise ValueError('fill_holes_and_remove_small_masks takes 2D or 3D array, not %dD array'%masks.ndim)
+    # slices = find_objects(masks)
+    # j = 0
+    # for i,slc in enumerate(slices):
+    #     if slc is not None:
+    #         msk = masks[slc] == (i+1)
+    #         npix = msk.sum()
+    #         if min_size > 0 and npix < min_size:
+    #             masks[slc][msk] = 0
+    #         else:    
+    #             if msk.ndim==3:
+    #                 for k in range(msk.shape[0]):
+    #                     msk[k] = binary_fill_holes(msk[k])
+    #             else:
+    #                 msk = binary_fill_holes(msk)
+    #             masks[slc][msk] = (j+1)
+    #             j+=1
+    # return masks
