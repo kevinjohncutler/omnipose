d82c29cdc3a200515a9d09c38c866bfd2e2071f7
manselme <marie.anselmet@pasteur.fr>
2025-02-17 17:32:22 +0100

    remove torchvision import from pyproject.toml

--- omnipose/_version.py
+++ omnipose/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev370+gd27bb8c'
-__version_tuple__ = version_tuple = (0, 1, 'dev370', 'gd27bb8c')
+__version__ = version = '0.1.dev373+g5728cb6.d20250217'
+__version_tuple__ = version_tuple = (0, 1, 'dev373', 'g5728cb6.d20250217')
