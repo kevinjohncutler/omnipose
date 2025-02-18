8c4dc4e578625a016b3b63a3f8d0eea6054c765a
manselme <marie.anselmet@pasteur.fr>
2025-02-18 11:33:34 +0100

    add test training

--- test_training.py
+++ test_training.py
@@ -1,28 +1,40 @@
+import os
 import numpy as np
+import torch
 from cellpose_omni import models
 
 def test_training():
-    model = models.CellposeModel(gpu=False, model_type='bact_phase_omni', net_avg=False, 
-                                 diam_mean=0., nclasses=4, dim=2, nchan=1)
+    model = models.CellposeModel(gpu=False, omni=True, nclasses=4, nchan=1, diam_mean=0)
 
     train_images = [np.ones((512, 512), dtype=np.float32) for _ in range(2)]
     train_masks = [np.zeros((512, 512), dtype=np.int32) for _ in range(2)]
-    
+    train_links = [
+        [(1, 2), (3, 4)],
+        [(5, 6), (7, 8)]
+    ]
+
     for i in range(2):
-        train_images[i][3:100, 2:40, 0] = 217
-        train_images[i][225:300, 60:80, 0] = 398
-        train_masks[i][3:100, 2:40, 0] = 1
+        train_images[i][3:100, 2:40] = 217
+        train_images[i][225:300, 60:80] = 398
+        train_masks[i][3:100, 2:40] = 1
     
-    params = {
-        'learning_rate': 0.001,
-        'n_epochs': 2,
-        'batch_size': 1,
-        'rescale': None,
-        'save_path': './',
-        'save_every': 1
-    }
+    save_dir = os.path.dirname(__file__)
     
-    model.train(train_data=train_images, train_labels=train_masks, train_links=[], **params)
+    model.train(train_images, train_masks, train_links, save_every=2, n_epochs=2, batch_size=1, 
+                save_path=save_dir,
+                channels=[0,0], min_train_masks=1, rescale=0)
     
-    import os
-    assert os.path.exists('./cellpose_model.pth'), "Model file was not saved"
+    model_path = None
+    models_dir = os.path.join(save_dir, "models")
+    for file_name in os.listdir(models_dir):
+        if file_name.endswith("epoch_1"):
+            model_path = os.path.join(models_dir, file_name)
+            break
+
+    assert model_path is not None, "Model checkpoint for epoch 1 not saved"
+
+    model_file = torch.load(model_path)
+    assert isinstance(model_file, dict), "Loaded model file is not a dictionary"
+    assert any(key.startswith('downsample') or key.startswith('output') for key in model_file.keys()), "State dict does not contain weights values"
+
+
