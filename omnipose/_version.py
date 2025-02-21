5f9afc9ab6fe714525c966d934e221e8979b2197
manselme <marie.anselmet@pasteur.fr>
2025-02-21 15:45:51 +0100

    update upload

--- omnipose/_version.py
+++ omnipose/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev391+gd5d8ad1.d20250219'
-__version_tuple__ = version_tuple = (0, 1, 'dev391', 'gd5d8ad1.d20250219')
+__version__ = version = '0.1.dev399+gd7df2ef.d20250221'
+__version_tuple__ = version_tuple = (0, 1, 'dev399', 'gd7df2ef.d20250221')
